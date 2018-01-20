2ac02c178903d1ee7df121362a9879cdb025e8fb
Luis Fernando Hernández Morales <163189@ids.upchiapas.edu.mx>
2018-01-20 13:50:47 -0600

    ....

--- JS/Engine.js
+++ JS/Engine.js
@@ -1,21 +1,36 @@
 $(document).ready(function(){
+	"use strict";
+	var showing = false;
     $("#Desktop").hide();
-    //$("#Desktop").show();
+	$("#TaskBar").hide();
     //Mostramos únicamente el div de lockscreen
-    
-    //Desenfocar el fondo de bloqueo
-    
-    
-    
     $("#login").click(function(e){
+		e.preventDefault();
+		$("#login-fx").get(0).play();
         $("#LockScreen").fadeOut(1000,function(){
             $("#LockScreen").hide();
-            
             //Mostrar el escritorio
             $("#Desktop").fadeIn(1000,function(){
                 $("#Desktop").show();
+				$("#TaskBar").show();
             }).fadeIn(1000);
             
         }).fadeOut(1000);
     });
+	
+	//Abrir el menú de inicio
+	$("#home-btn").click(function(e){
+		e.preventDefault();
+		if(!showing){
+			$("#menu-div").animate({
+				'left':'0'
+			},500);	
+			showing = true;
+		}else{
+			$("#menu-div").animate({
+				'left':'-900'
+			},500);
+			showing = false;
+		}
+	});
 });
